e7fb2aac46c3e211b4a6af9dbb0279d87d6ca585
Jan Cajthaml <jan.cajthaml@gmail.com>
2017-08-03 21:48:31 +0200

    more compact code

--- luhn.go
+++ luhn.go
@@ -1,13 +1,16 @@
 package main
 
-import (
-	"os"
-)
-
-var noop = []byte{}
+import "os"
 
 var m = [...]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 6, 8, 1, 3, 5, 7, 9}
 
+/**
+ * Luhn Checksum algorithm
+ *
+ * @see https://en.wikipedia.org/wiki/Luhn_algorithm
+ *
+ * @author jan.cajthaml
+ */
 func Luhn(cc string) (ok bool) {
 	var (
 		i int = len(cc) - 1
@@ -17,20 +20,23 @@ func Luhn(cc string) (ok bool) {
 	for i >= 0 {
 		switch i & 1 {
 		case 1:
+			// [0 => 0]
 			x += int(cc[i]) - 48
 		default:
+			// lookup [0..4 => x<<1], [5..8 => (x<<1)+1]
 			x += m[int(cc[i])]
 		}
 		i--
 	}
 
+	// int32 mod 10
 	return (x - (x/10)*10) == 0
 }
 
 func main() {
 	defer func() {
 		if recover() != nil {
-			os.Stderr.Write(noop)
+			os.Exit(1)
 		}
 	}()
 
@@ -40,9 +46,9 @@ func main() {
 	}
 
 	if !Luhn(os.Args[1]) {
-		os.Stdout.Write(noop)
+		os.Exit(1)
 		return
 	}
 
-	os.Stdout.Write(noop)
+	os.Exit(0)
 }
