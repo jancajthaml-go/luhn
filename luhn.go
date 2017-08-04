d9bb4adfbb5e1776fd3d670732da23ea2e23c744
Jan Cajthaml <jan.cajthaml@gmail.com>
2017-08-04 07:28:03 +0200

    improved performance from 16.8 ns/op to 8.28 ns/op using goto instead of for-loop

--- luhn.go
+++ luhn.go
@@ -1,6 +1,6 @@
 package main
 
-var m = [...]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 6, 8, 1, 3, 5, 7, 9}
+var m = [58]uint{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 4, 6, 8, 1, 3, 5, 7, 9}
 
 /**
  * Luhn Checksum algorithm
@@ -11,23 +11,23 @@ var m = [...]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
  */
 func Luhn(cc string) (ok bool) {
 	var (
-		i int = len(cc) - 1
-		x int = 0
+		i int  = len(cc) - 1
+		x uint = 0
 	)
 
-	for i >= 0 {
-		switch i & 1 {
-		case 1:
-			// rune(x) => atoi(x)
-			x += int(cc[i]) - 48
-		default:
-			// rune(x) => atoi(y:0..4) => y * 2
-			//         => atoi(y:5..8) => y * 2 + 1
-			x += m[int(cc[i])]
-		}
-		i--
+scan:
+	switch i & 1 {
+	case 1:
+		x += uint(cc[i]) - 48
+	default:
+		x += m[int(cc[i])]
 	}
+	if i == 0 {
+		goto eos
+	}
+	i--
+	goto scan
 
-	// mod10
+eos:
 	return (x - (x/10)*10) == 0
 }
